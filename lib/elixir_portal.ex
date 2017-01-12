1b84b6577c264d52ddf56ceac6864cc7d55c8c29
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-11 16:15:01 -0800

    Initial commit.

---
+++ lib/elixir_portal.ex
@@ -0,0 +1,18 @@
+defmodule ElixirPortal do
+  @moduledoc """
+  Documentation for ElixirPortal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirPortal.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_portal_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirPortalTest do
+  use ExUnit.Case
+  doctest ElixirPortal
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
